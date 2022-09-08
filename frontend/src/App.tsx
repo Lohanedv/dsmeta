dda066177952927ec9283d4c9e30890cc953622d
Lohane Dafne Borges Vieira <lohanedafnevieira@gmail.com>
2022-09-08 12:27:58 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
-  return(
-    
-  )
+  return (
+    <>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
